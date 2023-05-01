82485b9aa1aa8fa85940fdc662efd49bc6715d15
Jeremy Nixon <jnixon2@gmail.com>
2023-05-01 07:33:28 +0000

    update general paths

--- app.py
+++ app.py
@@ -14,11 +14,8 @@ import base64
 import io
 import faiss
 import pickle
-<<<<<<< HEAD
-=======
 import time
 from transformers import CLIPProcessor, CLIPModel
->>>>>>> bb3cc5d (make code run on one machine)
 
 app = Flask(__name__)
 clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
@@ -57,7 +54,7 @@ def search(embedding, k=5):
 
 def find_nearest_paths(input_path, k=100):    
     # Get the embedding for the input path
-    input_path = '/home/ga/static/' + input_path
+    input_path = os.path.join(os.getcwd(), 'static/') + input_path
     input_embedding = np_embeddings[path_to_index[input_path]]
     # Perform the search
     distances, indexes = search(input_embedding, k)
@@ -65,8 +62,6 @@ def find_nearest_paths(input_path, k=100):
     nearest_paths = [index_to_path[idx] for idx in indexes.flatten()]
     return [p.split('static/')[1] for p in nearest_paths]
 
-<<<<<<< HEAD
-=======
 def embed_text(text):
     inputs = processor(text=texts, return_tensors="pt", padding=True, truncation=True)
     with torch.no_grad():
@@ -87,14 +82,12 @@ def query_search():
     return render_template('similar_images.html', images=similar_image_paths, weighted_rand=weighted_rand)
 
 
->>>>>>> bb3cc5d (make code run on one machine)
 @app.route('/similar-images')
 def similar_images():
     image_path = request.args.get('image_path')
     print("Backend image path recieved:", image_path, flush=True)
     similar_image_paths = find_nearest_paths(image_path)
     return render_template('similar_images.html', images=similar_image_paths, weighted_rand=weighted_rand)
-
 
 @app.route('/nearest_images')
 def nearest_images():
@@ -104,15 +97,7 @@ def nearest_images():
 
 @app.route('/')
 def index():
-    img_dir = '/home/ga/static/instagram_training_data/'
-    img_paths = os.listdir(img_dir)[:300]
-    as_np = np.array(find_images_in_folder(img_dir))
-    chosen_indices = as_np[np.random.randint(0, len(as_np), 500)]
-    images = list(chosen_indices)
-    print("Images", images[:5])
-    nearest_paths = find_nearest_paths(images[0][0])
-    print("Nearest paths", nearest_paths)
-    return render_template('index.html', images=nearest_paths, weighted_rand=weighted_rand)
+    return similar_images()
 
 if __name__ == '__main__':
     app.run(debug=True)
